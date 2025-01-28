bea87a82eb26b8688bdedba8211ff87c0725cf40
Fernando <cbm0100687arriaga@gmail.com>
2025-01-28 12:15:08 -0600

    Agregando archvioss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+const config = {
+	content: [
+	  './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+	  './node_modules/flowbite/**/*.js', // Agrega esta línea
+	],
+	theme: {
+	  extend: {},
+	},
+	plugins: [
+	  require('flowbite/plugin'), // Agrega esta línea
+	],
+  };
+  
+  export default config;
