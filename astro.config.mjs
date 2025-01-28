90dbd0567c8d3f0de1375533916cd4afc4b4ea2a
Fernando <cbm0100687arriaga@gmail.com>
2025-01-27 19:37:04 -0600

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig({
+export default defineConfig({ outDir: './docs',
   integrations: [tailwind()]
 });
